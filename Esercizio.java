e5ec1df722516c4b33fb18b858ed373c4baa7f9a
SimMan123 <simone.manzari2@marconibari.edu.it>
2024-11-27 09:40:14 +0000

    vector1

--- Esercizio.java
+++ Esercizio.java
@@ -1,26 +1,60 @@
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
 
-//Import di Classi Java necessarie al funzionamento del programma
 import java.util.Scanner;
 
-// Classe principale, con metodo main
+
 class Esercizio {
-    // Il programma parte con una chiamata a main().
     public static void main(String args[])
     {
-        //Variabili del programma
-        String nome;
-
-        //Creo l'oggetto in per l'input da tastiera
         Scanner in = new Scanner( System.in );
-
-        //Leggo l'input da tastiera
-        System.out.print("Inserisci il tuo nome: ");
-        nome = in.nextLine();
-
-        //Output del nome acquisito da tastiera
-        System.out.println("Ciao "+nome+"!");
+        String v[] = new   v[5]; 
+        v[0]= "g";
+        v[1]= "m";
+        v[2]= "h";
+        v[3]= "f";
+        v[4]= "d";
+      String w[] = new  w[5];
+      int i=0;
+      for (i=0;i<=4;i++) {
+        System.out.print("inserire nome cantante");
+        w[i]= in.nextLine();
+      }
+      int x[] = new x[10];
+      for ( i=0; i<=9; i++) {
+        x[i]=i+1;
+      }
+      int y[]= new y[10];
+      for (i=0;i<10;i++) {
+        y[i]= (i+1)*2;
+      }
+      System.out.print("visuallizzare solo 3 elementi del vettore v" + v[0] + v[2] + v[4]);
+      System.out.print("visuallizzre tutti gli elementi del vettore w,v,y,x");
+      System.out.println("v:");
+      for(i=0;i<=4;i++) {
+        sYSTEM.OUT.println(v[i]) ;
+        System.out.prinln("w:");
+        for(i=0;i<=4;i++) {
+            sYSTEM.OUT.println(w[i]) ;
+      }
+      System.out.println("y:");
+      for(i=0;i<=9;i++) {
+        System.out.println(y[i]);
+      }
+      System.out.println("x:");
+      while (i<=9) {
+        System.out.println(x[i]);
+        i=i+2;
+      }
+      i=1;
+      while (i<=9) {
+        System.out.println(x[i]);
+        i=i+2;
+      }
+      while (i<=9) {
+        if (x[i]%2==0) {
+        System.out.println(x[i]);
+        }
+        i=i+1;
+      }
     }
 }
 
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
